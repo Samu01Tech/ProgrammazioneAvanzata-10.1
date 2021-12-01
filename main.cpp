de25c08b7655c8c1ab98faf283ade421c15ce7fb
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-12-01 14:44:32 +0000

    init

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
+void stampa(int dato){
+    cout << dato << " " << endl;
+}
+
 int main() {
-    cout << "Hello World!\n";
+    vector<int> v = {1, 5, 2, 3, 0};
+    
+    for_each(v.begin(), v.end(),)
+    return 0;
 } 
